aa684f1cfb2b276e193d18abce9d4d939d3bd736
Gijsjan <agijsbro@gmail.com>
2013-06-06 12:19:40 +0200

    added testing

--- app/index.js
+++ app/index.js
@@ -2,11 +2,15 @@
 var util = require('util');
 var path = require('path');
 var yeoman = require('yeoman-generator');
-var fs = require('fs');
-
 
 var DefaultGenerator = module.exports = function DefaultGenerator(args, options, config) {
 	yeoman.generators.Base.apply(this, arguments);
+
+	// console.log(this);
+	this.hookFor('backbone-amd:testing', {
+		args: args,
+		options: options
+	});
 
 	this.on('end', function () {
 		this.installDependencies({ skipInstall: options['skip-install'] });
@@ -36,43 +40,47 @@ DefaultGenerator.prototype.askFor = function askFor() {
 	}.bind(this));
 };
 
-DefaultGenerator.prototype.create = function app() {
+var base = 'dev/';
+
+DefaultGenerator.prototype.createBase = function app() {
+
+	this.mkdir('test');
+	this.template('test/body.coffee', 'test/body.coffee');
+
+	this.mkdir(base);
+	this.mkdir(base + 'html');
+	this.mkdir(base + 'css');
+	this.mkdir(base + 'images');
+	this.mkdir(base + 'js');
+	this.mkdir(base + 'js/routers');
+	this.mkdir(base + 'js/models');
+	this.mkdir(base + 'js/collections');
+	this.mkdir(base + 'js/views');
+	this.mkdir(base + 'js/managers');
+
 	this.template('_bowerrc', '.bowerrc');
 	this.template('bower.json', 'bower.json');
 	this.template('package.json', 'package.json');
+	this.template('Gruntfile.coffee', 'Gruntfile.coffee');
+};
 
-	this.mkdir('html');
-	this.template('index.jade', 'index.jade');
-
-	this.mkdir('css');
-	this.template('css/main.styl', 'css/main.styl');
-
-	this.mkdir('images');
+DefaultGenerator.prototype.createMarkup = function app() {
+	this.template(base+'index.jade', base+'index.jade');
+	this.template(base+'css/main.styl', base+'css/main.styl');
 };
 
 DefaultGenerator.prototype.createJS = function app() {
-	this.mkdir('js');
-	this.template('js/main.coffee', 'js/main.coffee');
-	this.template('js/app.coffee', 'js/app.coffee');
-	this.template('js/config.coffee', 'js/config.coffee');
-	this.template('js/pubsub.coffee', 'js/pubsub.coffee');
-	
-	this.mkdir('js/routers');
-	this.template('js/routers/main.coffee', 'js/routers/main.coffee');
+	this.template(base+'js/main.coffee', base+'js/main.coffee');
+	this.template(base+'js/app.coffee', base+'js/app.coffee');
+	this.template(base+'js/config.coffee', base+'js/config.coffee');
+	this.template(base+'js/pubsub.coffee', base+'js/pubsub.coffee');
 
-	this.mkdir('js/models');
-	this.template('js/models/base.coffee', 'js/models/base.coffee');
-	this.template('js/models/currentUser.coffee', 'js/models/currentUser.coffee');
+	this.template(base+'js/routers/main.coffee', base+'js/routers/main.coffee');
 
-	this.mkdir('js/collections');
-	this.template('js/collections/base.coffee', 'js/collections/base.coffee');
+	this.template(base+'js/models/base.coffee', base+'js/models/base.coffee');
+	this.template(base+'js/models/currentUser.coffee', base+'js/models/currentUser.coffee');
 
-	this.mkdir('js/views');
-
-	this.mkdir('js/managers');
+	this.template(base+'js/collections/base.coffee', base+'js/collections/base.coffee');
 };
 
-// DefaultGenerator.prototype.projectfiles = function projectfiles() {
-//   // this.copy('editorconfig', '.editorconfig');
-//   // this.copy('jshintrc', '.jshintrc');
-// };
+// ADD helpers & managers using Grunt
--- app/templates/js/routers/main.coffee
+++
@@ -1,9 +0,0 @@
-define (require) ->
-	Backbone = require 'backbone'
-
-	class MainRouter extends Backbone.Router
-
-		'routes':
-			'': 'home'
-
-		home: ->
